b2b0b1f0064318d6268ddbdb065bc713817e917f
Ecthor <thorofwar@gmail.com>
2015-04-14 14:57:10 +0200

    Odbieranie danych

---
+++ scripts/listener_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+
+import rospy
+from std_msgs.msg import Float32MultiArray
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # node are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'talker' node so that multiple talkers can
+    # run simultaneously.
+    rospy.init_node('listener', anonymous=True)
+
+    rospy.Subscriber("float32MultiArray", Float32MultiArray, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
+
