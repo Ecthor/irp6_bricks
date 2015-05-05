0c2941adbf1643d1168ae7bfb54bd953f2cf47db
Ecthor <thorofwar@gmail.com>
2015-05-05 13:53:24 +0200

    Lapie niebieskiego, odklada pod siebie i wciska

--- scripts/listener_move.py
+++ scripts/listener_move.py
@@ -66,7 +66,7 @@ def scale_rotation(y,x):
 			dist_max_val = [x[i],x[i+1],y[i],y[i+1]]
 	#print 'Scale: ' + str(dist_min) + ' = 3,1cm'
 	move_x=((655-central_pos(x))*3.1)/dist_min #655
-	move_y=((600-central_pos(y))*3.1)/dist_min #637
+	move_y=((652-central_pos(y))*3.1)/dist_min #637
 	#print 'Distance x: ' + str(move_x) + ' cm'
 	#print 'Distance y: ' + str(move_y) + ' cm'
 	move_y=-move_y/100
@@ -117,6 +117,62 @@ def listener():
 				break
 	# spin() simply keeps python from exiting until this node is stopped
 	#rospy.spin()
+	
+def move_over(move_x, move_y, rads):
+    irpos.move_rel_to_cartesian_pose_with_contact(17.0, Pose(Point(move_x, move_y, 0), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(9.0,9.0,9.0),Vector3(0.0,0.0,0.0)))
+    myjoint = irpos.get_joint_position()
+    lst = list(myjoint)
+    lst[5] = lst[5]-rads
+    myjoint = tuple(lst)
+    print myjoint
+    irpos.move_to_joint_position(myjoint, 8.0)
+    #move down to correct
+    irpos.move_rel_to_cartesian_pose_with_contact(5.0, Pose(Point(0, 0, 0.05), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(9.0,9.0,9.0),Vector3(0.0,0.0,0.0)))
+    return
+    
+def correction(move_x, move_y, rads):
+	if move_x<0.05:# and move_x>0.0005:
+		print "Correcting x"
+		irpos.move_rel_to_cartesian_pose_with_contact(6.0, Pose(Point(move_x, 0, 0), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(9.0,9.0,9.0),Vector3(0.0,0.0,0.0)))
+	if move_y<0.05:# and move_y>0.0005:
+		print "Correcting y"
+		irpos.move_rel_to_cartesian_pose_with_contact(6.0, Pose(Point(0, move_y, 0), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(9.0,9.0,9.0),Vector3(0.0,0.0,0.0)))
+	print rads
+	if math.fabs(rads)<1 and math.fabs(rads)>0.05:
+		print "Correcting rotation"
+		#TODO
+		
+def grab_brick():
+	irpos.tfg_to_joint_position(0.09, 5.0)
+	irpos.move_rel_to_cartesian_pose_with_contact(20.0, Pose(Point(0, 0, 0.3), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(7.0,7.0,7.0),Vector3(0.0,0.0,0.0)))
+	irpos.move_rel_to_cartesian_pose_with_contact(6.0, Pose(Point(0, 0, -0.005), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(9.0,9.0,9.0),Vector3(0.0,0.0,0.0)))
+	irpos.tfg_to_joint_position(0.073, 5.0)
+	irpos.move_rel_to_cartesian_pose_with_contact(10.0, Pose(Point(0, 0, -0.2), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(9.0,9.0,9.0),Vector3(0.0,0.0,0.0)))
+	irpos.move_to_joint_position([ 7.412760409739285e-06, -1.764427006069524, 0.0006186793623569331, 0.1930235079212923, 4.7123619308455735, 1.5707923033898181], 10.0)
+	
+def put_brick():
+	irpos.move_rel_to_cartesian_pose_with_contact(20.0, Pose(Point(0, 0, 0.3), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(6.0,6.0,6.0),Vector3(0.0,0.0,0.0)))
+	irpos.move_rel_to_cartesian_pose_with_contact(3.0, Pose(Point(0, 0, -0.005), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(6.0,6.0,6.0),Vector3(0.0,0.0,0.0)))
+	print "Open"
+	irpos.tfg_to_joint_position(0.09, 5.0)
+	irpos.move_rel_to_cartesian_pose_with_contact(5.0, Pose(Point(0, 0, -0.05), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(9.0,9.0,9.0),Vector3(0.0,0.0,0.0)))
+	
+def push_brick():
+	irpos.tfg_to_joint_position(0.055, 5.0)
+	irpos.move_rel_to_cartesian_pose_with_contact(7.0, Pose(Point(0, 0, 0.08), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(6.0,6.0,6.0),Vector3(0.0,0.0,0.0)))
+	irpos.move_rel_to_cartesian_pose(5.0, Pose(Point(0.005, 0.005, 0), Quaternion(0.0, 0.0, 0.0, 1.0)))
+	irpos.move_rel_to_cartesian_pose(5.0, Pose(Point(-0.005, -0.005, 0), Quaternion(0.0, 0.0, 0.0, 1.0)))
+	irpos.move_rel_to_cartesian_pose_with_contact(7.0, Pose(Point(0, 0, 0.08), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(6.0,6.0,6.0),Vector3(0.0,0.0,0.0)))
+	#go up
+	irpos.move_rel_to_cartesian_pose_with_contact(5.0, Pose(Point(0, 0, -0.05), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(9.0,9.0,9.0),Vector3(0.0,0.0,0.0)))
+		
+def service_position():
+	irpos.move_to_joint_position([ 7.412760409739285e-06, -1.764427006069524, 0.0006186793623569331, 0.1930235079212923, 4.7123619308455735, 1.5707923033898181], 10.0)
+	irpos.tfg_to_joint_position(0.09, 5.0)
+	rospy.sleep(2)
+	
+		
+	
     
 def move_operation():
 	global first_time
@@ -133,56 +189,27 @@ def move_operation():
 	scale_rotation(data[1:5],data[5:9])
 	if first_time==0:
 		print "FIRST STAGE"
-		irpos.move_rel_to_cartesian_pose_with_contact(17.0, Pose(Point(move_x, move_y, 0), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(9.0,9.0,9.0),Vector3(0.0,0.0,0.0)))
-		myjoint = irpos.get_joint_position()
-		lst = list(myjoint)
-		lst[5] = lst[5]-rads
-		myjoint = tuple(lst)
-		print myjoint
-		irpos.move_to_joint_position(myjoint, 8.0)
-		#print "Correct"
-		irpos.move_rel_to_cartesian_pose_with_contact(5.0, Pose(Point(0, 0, 0.05), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(9.0,9.0,9.0),Vector3(0.0,0.0,0.0)))
+		move_over(move_x, move_y, rads)
 		first_time=first_time+1
 		rospy.sleep(2)
 		return
 		
 	if first_time==1:
 		print "SECOND STAGE"
-		if move_x<0.05:# and move_x>0.0005:
-			print "Correcting x"
-			irpos.move_rel_to_cartesian_pose_with_contact(6.0, Pose(Point(move_x, 0, 0), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(9.0,9.0,9.0),Vector3(0.0,0.0,0.0)))
-		if move_y<0.05:# and move_y>0.0005:
-			print "Correcting y"
-			irpos.move_rel_to_cartesian_pose_with_contact(6.0, Pose(Point(0, move_y, 0), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(9.0,9.0,9.0),Vector3(0.0,0.0,0.0)))
-		print rads
-		if math.fabs(rads)<1 and math.fabs(rads)>0.05:
-			print "Correcting rotation"
+		correction(move_x, move_y, rads)
 		first_time=first_time+1
 		return
 		
 	if first_time==2:
 		print "THIRD STAGE"
-		irpos.tfg_to_joint_position(0.09, 5.0)
-		irpos.move_rel_to_cartesian_pose_with_contact(20.0, Pose(Point(0, 0, 0.3), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(7.0,7.0,7.0),Vector3(0.0,0.0,0.0)))
-		irpos.move_rel_to_cartesian_pose_with_contact(6.0, Pose(Point(0, 0, -0.005), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(9.0,9.0,9.0),Vector3(0.0,0.0,0.0)))
-		irpos.tfg_to_joint_position(0.073, 5.0)
-		irpos.move_rel_to_cartesian_pose_with_contact(10.0, Pose(Point(0, 0, -0.2), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(9.0,9.0,9.0),Vector3(0.0,0.0,0.0)))
-		irpos.move_to_joint_position([ 7.412760409739285e-06, -1.764427006069524, 0.0006186793623569331, 0.1930235079212923, 4.7123619308455735, 1.5707923033898181], 10.0)
+		grab_brick()
 		rospy.sleep(2)
 		#Bring back the block
-		irpos.move_rel_to_cartesian_pose_with_contact(20.0, Pose(Point(0, 0, 0.3), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(6.0,6.0,6.0),Vector3(0.0,0.0,0.0)))
-		irpos.move_rel_to_cartesian_pose_with_contact(3.0, Pose(Point(0, 0, -0.005), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(6.0,6.0,6.0),Vector3(0.0,0.0,0.0)))
-		irpos.tfg_to_joint_position(0.09, 5.0)
-		irpos.move_rel_to_cartesian_pose_with_contact(5.0, Pose(Point(0, 0, -0.05), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(9.0,9.0,9.0),Vector3(0.0,0.0,0.0)))
-		irpos.tfg_to_joint_position(0.055, 5.0)
-		irpos.move_rel_to_cartesian_pose_with_contact(7.0, Pose(Point(0, 0, 0.08), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(6.0,6.0,6.0),Vector3(0.0,0.0,0.0)))
-		irpos.move_rel_to_cartesian_pose_with_contact(5.0, Pose(Point(0, 0, -0.05), Quaternion(0.0, 0.0, 0.0, 1.0)), Wrench(Vector3(9.0,9.0,9.0),Vector3(0.0,0.0,0.0)))
-		irpos.move_to_joint_position([ 7.412760409739285e-06, -1.764427006069524, 0.0006186793623569331, 0.1930235079212923, 4.7123619308455735, 1.5707923033898181], 10.0)
-		
-		
-		
-		
-		
+		put_brick()
+		#push
+		push_brick()
+		#return to service position
+		service_position()
 		first_time=first_time+1
 		return
 		
@@ -191,8 +218,7 @@ def move_operation():
 		
 if __name__ == '__main__':
 	irpos = IRPOS("thIRpOS", "Irp6p", 6, 'irp6p_manager') #z csn
-	irpos.move_to_joint_position([ 7.412760409739285e-06, -1.764427006069524, 0.0006186793623569331, 0.1930235079212923, 4.7123619308455735, 1.5707923033898181], 10.0)
-	rospy.sleep(2)
+	service_position()
 	rospy.Subscriber("float32MultiArray", Float32MultiArray, callback)
 	listener()
 
